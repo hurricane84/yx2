741809a33fbdd9810fe59c58ffa3e41bcce552c0
hurricane84 <2819824911@qq.com>
2022-08-08 17:05:48 +0800

    initial commit

---
+++ src/views/Demo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="dialog">
+        <div class="dialog-title">标题</div>
+        <div class="dialog-content">内容</div>
+    </div>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+$color: #f00;
+@mixin wyy(){
+    width: 200px;
+    height: 200px;
+}
+.dialog{
+    // width: 200px;
+    // height: 200px;
+    @include wyy();
+    background: $color;
+    &-title{
+        color: #fff;
+    }
+}
+@function style($arg){
+    @if($arg == 1){
+        @return #000;
+    }
+    @else if($arg == 2){
+        @return #fff;
+    }
+    @else{
+        @return #ccc;
+    }
+}
+.box{
+    background: style(5);
+}
+// eslint-disable-next-line eol-last
+</style>
