0f952a8728e3bcb3632e556d2293d9bafde15047
hurricane84 <2819824911@qq.com>
2022-08-09 00:47:15 +0800

    项目初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Demo from '../views/Demo.vue'
+import Home from '../views/Home.vue'
+import Category from '../views/Category.vue'
+import Cart from '../views/Cart.vue'
+import Profile from '../views/Profile.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +13,22 @@ const routes = [
     path: '/demo',
     name: 'Demo',
     component: Demo
+  }, {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/category',
+    name: 'Category',
+    component: Category
+  }, {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  }, {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
   }
 ]
 
